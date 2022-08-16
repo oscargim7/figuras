027e7519510ad5e00d263f00160e1a8d3deae9e2
oscargim7 <oscarromero1601@gmail.com>
2022-08-15 20:51:21 -0500

    Cuadrado

---
+++ cuadrado.cpp
@@ -0,0 +1,41 @@
+/*
+    Romero Escamilla Oscar Eduardo
+    22110112
+    2P
+    Programación Orientada en Objetos
+    Descripción de programa:
+        - Dibujado de un cuadrado en C++.
+*/
+
+class Cuadrado
+{
+    //Atributos
+    private:
+        int fila, columna; 
+
+    //Métodos
+     public: 
+        draw();
+}
+
+int main()
+{
+    //Creación de objeto
+    Cuadrado micuadrado;
+
+    cout<< "Ingrese el numero de filas";
+    cin>>micuadrado.fila;
+
+    cout<< "Ingrese el numero de columnas";
+    cin>>micuadrado.columna;
+    
+    for(int i = 0; i<micuadrado.fila; i++)
+    {
+        for(int j = 0; j<micuadrado.fila; j++)
+        {
+            cout<<"*";
+            micuadrado.draw();
+        }
+    }
+    
+}
